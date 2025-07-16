d65c50901823f76a5b79a0d0cebba4e705b337b9
WindTunnelRetirement <ishii@dentec.co.jp>
2025-07-16 13:05:47 +0900

    botの修正

--- app/controllers/typing_problems_controller.rb
+++ app/controllers/typing_problems_controller.rb
@@ -2,6 +2,7 @@ class TypingProblemsController < ApplicationController
   def index
     @typing_problems = TypingProblem.all
   end
+
   def play
     @typing_problem = TypingProblem.find(params[:id])
     @next_problem = TypingProblem.where("id > ?", @typing_problem.id).first
