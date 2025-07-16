6899cdc1ef14515c4cc658665103e6c7a4c2c03c
WindTunnelRetirement <ishii@dentec.co.jp>
2025-07-16 13:53:53 +0900

    rubocop実行修正

--- app/models/typing_problem.rb
+++ app/models/typing_problem.rb
@@ -1,2 +1,4 @@
+# frozen_string_literal: true
+
 class TypingProblem < ApplicationRecord
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file should ensure the existence of records required to run the application in every environment (production,
 # development, test). The code here should be idempotent so that it can be executed at any point in every environment.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
@@ -9,7 +11,7 @@
 #   end
 
 TypingProblem.create([
-  { content: "The quick brown fox jumps over the lazy dog." },
-  { content: "Hello, world! This is a typing test." },
-  { content: "Ruby on Rails makes web development fun and productive." }
+  { content: 'The quick brown fox jumps over the lazy dog.' },
+  { content: 'Hello, world! This is a typing test.' },
+  { content: 'Ruby on Rails makes web development fun and productive.' }
 ])
